54afb32d746b8a4e3cca757fd45daf5067d32245
David Allison <david.s.allison@gmail.com>
2025-08-27 15:27:53 -0700

    Client implementation of server streaming

--- rpc/server/rpc_server.cc
+++ rpc/server/rpc_server.cc
@@ -4,6 +4,12 @@
 #include <stdio.h>
 
 namespace subspace {
+
+using Method = internal::Method;
+using Session = internal::Session;
+using MethodInstance = internal::MethodInstance;
+using AnyStreamWriter = internal::AnyStreamWriter;
+
 RpcServer::RpcServer(std::string service_name,
                      std::string subspace_server_socket)
     : name_(std::move(service_name)),
@@ -117,8 +123,8 @@ absl::Status RpcServer::RegisterMethod(
 absl::Status RpcServer::RegisterMethod(
     const std::string &method, const std::string &request_type,
     const std::string &response_type, int32_t slot_size, int32_t num_slots,
-    std::function<absl::Status(const google::protobuf::Any &,
-                               std::function<absl::Status(std::unique_ptr<google::protobuf::Any>)>, co::Coroutine *)>
+    std::function<absl::Status(const google::protobuf::Any &, AnyStreamWriter &,
+                               co::Coroutine *)>
         callback,
     int id) {
   if (methods_.find(method) != methods_.end()) {
@@ -343,6 +349,12 @@ absl::Status RpcServer::HandleOpen(uint64_t client_id,
     res->set_name(absl::StrFormat("%s/%d/%d", method->method->response_channel,
                                   client_id, session->session_id));
     res->set_type(method->method->response_type);
+
+    if (method->method->IsStreaming()) {
+      m->set_cancel_channel(absl::StrFormat("%s/%d/%d",
+                                            method->method->cancel_channel,
+                                            client_id, session->session_id));
+    }
   }
   return absl::OkStatus();
 }
@@ -364,7 +376,7 @@ absl::Status RpcServer::HandleClose(uint64_t client_id,
   return absl::OkStatus();
 }
 
-absl::StatusOr<std::shared_ptr<RpcServer::Session>>
+absl::StatusOr<std::shared_ptr<Session>>
 RpcServer::CreateSession(uint64_t client_id) {
   auto session = std::make_shared<Session>();
   session->session_id = ++next_session_id_;
@@ -402,16 +414,18 @@ RpcServer::CreateSession(uint64_t client_id) {
     method_instance->response_publisher =
         std::make_shared<subspace::Publisher>(std::move(*pub));
 
-        // For a streaming method we create a cancel channel subscriber.
+    // For a streaming method we create a cancel channel subscriber.
     if (method->IsStreaming()) {
-      absl::StatusOr<subspace::Subscriber> cancel_sub = client_->CreateSubscriber(
-          absl::StrFormat("%s/%d/%d", method->cancel_channel, session->client_id,
-                          session->session_id),
-          {.reliable = true, .type = "subspace.RpcCancelRequest"});
+      absl::StatusOr<subspace::Subscriber> cancel_sub =
+          client_->CreateSubscriber(
+              absl::StrFormat("%s/%d/%d", method->cancel_channel,
+                              session->client_id, session->session_id),
+              {.reliable = true, .type = "subspace.RpcCancelRequest"});
       if (!cancel_sub.ok()) {
         logger_.Log(toolbelt::LogLevel::kError,
                     "Failed to create cancel subscriber for method %s: %s",
-                    method->name.c_str(), cancel_sub.status().ToString().c_str());
+                    method->name.c_str(),
+                    cancel_sub.status().ToString().c_str());
         return cancel_sub.status();
       }
       method_instance->cancel_subscriber =
@@ -425,7 +439,7 @@ RpcServer::CreateSession(uint64_t client_id) {
                       method_instance](co::Coroutine *c) {
           if (method_instance->method->IsStreaming()) {
             SessionStreamingMethodCoroutine(std::move(server), session,
-                                             method_instance, c);
+                                            method_instance, c);
           } else {
             SessionMethodCoroutine(std::move(server), session, method_instance,
                                    c);
@@ -489,6 +503,7 @@ void RpcServer::SessionMethodCoroutine(
     if (!request_ok) {
       continue;
     }
+
     subspace::RpcResponse response;
     response.set_session_id(session->session_id);
     response.set_request_id(request.request_id());
@@ -569,6 +584,283 @@ void RpcServer::SessionMethodCoroutine(
 void RpcServer::SessionStreamingMethodCoroutine(
     std::shared_ptr<RpcServer> server, std::shared_ptr<Session> session,
     std::shared_ptr<MethodInstance> method_instance, co::Coroutine *c) {
+  while (server->running_) {
+    auto s = method_instance->request_subscriber->Wait(
+        server->interrupt_pipe_.ReadFd(), c);
+    if (!s.ok()) {
+      server->logger_.Log(toolbelt::LogLevel::kError,
+                          "Error waiting for request: %s",
+                          s.status().ToString().c_str());
+      return;
+    }
+    if (*s == server->interrupt_pipe_.ReadFd().Fd()) {
+      break;
+    }
+    subspace::RpcRequest request;
+    bool request_ok = false;
+    for (;;) {
+      auto m = method_instance->request_subscriber->ReadMessage();
+      if (!m.ok()) {
+        server->logger_.Log(toolbelt::LogLevel::kError,
+                            "Error reading message for method %s: %s",
+                            method_instance->method->name.c_str(),
+                            m.status().ToString().c_str());
+        break;
+      }
+      if (m->length == 0) {
+        // No message, continue waiting.
+        break;
+      }
+      if (!request.ParseFromArray(m->buffer, m->length)) {
+        server->logger_.Log(toolbelt::LogLevel::kError,
+                            "Error parsing request for method %s: %s",
+                            method_instance->method->name.c_str(),
+                            m.status().ToString().c_str());
+        continue;
+      }
+      if (request.session_id() == session->session_id) {
+        request_ok = true;
+        break;
+      }
+    }
+    if (!request_ok) {
+      continue;
     }
 
+    server->logger_.Log(toolbelt::LogLevel::kDebug, "Calling method %s",
+                        method_instance->method->name.c_str());
+
+    AnyStreamWriter writer(server, session, method_instance, request);
+
+    // Start a coroutine to read the cancellation channel and cancel the
+    // StreamWriter if a cancellation request is received.
+    server->AddCoroutine(std::make_unique<co::Coroutine>(
+        *server->scheduler_, [server, session, method_instance, &writer,
+                              &request](co::Coroutine *c) {
+          toolbelt::FileDescriptor interrupt(
+              dup(server->interrupt_pipe_.ReadFd().Fd()));
+          while (!writer.IsCancelled()) {
+            auto s = method_instance->cancel_subscriber->Wait(interrupt, c);
+            if (!s.ok()) {
+              server->logger_.Log(toolbelt::LogLevel::kError,
+                                  "Error waiting for cancel: %s",
+                                  s.status().ToString().c_str());
+              return;
+            }
+            if (*s == interrupt.Fd()) {
+              break;
+            }
+            bool request_ok = false;
+            while (!request_ok) {
+              auto msg = method_instance->cancel_subscriber->ReadMessage();
+              if (!msg.ok()) {
+                server->logger_.Log(toolbelt::LogLevel::kError,
+                                    "Error reading cancel message: %s",
+                                    msg.status().ToString().c_str());
+                continue;
+              }
+              if (msg->length == 0) {
+                // No message, continue waiting.
+                break;
+              }
+              RpcCancelRequest cancel;
+              if (!cancel.ParseFromArray(msg->buffer, msg->length)) {
+                server->logger_.Log(toolbelt::LogLevel::kError,
+                                    "Error parsing cancel message: %s",
+                                    msg.status().ToString().c_str());
+                continue;
+              }
+              std::cerr << "CANCEL " << cancel.DebugString();
+              if (cancel.session_id() == session->session_id &&
+                  cancel.request_id() == request.request_id()) {
+                request_ok = true;
+                break;
+              }
+            }
+            if (request_ok) {
+              writer.Cancel();
+            }
+          }
+        }));
+
+    // Call the method and pass it StreamWriter that will be called to send back
+    // a response to the client.
+    absl::Status method_status =
+        method_instance->method->stream_callback(request.argument(), writer, c);
+    // If the method fails, we need to send an error response.
+    if (!method_status.ok()) {
+      server->logger_.Log(toolbelt::LogLevel::kError,
+                          "Error executing method %s: %s",
+                          method_instance->method->name.c_str(),
+                          method_status.ToString().c_str());
+      SendRpcError(server, session, method_instance, request,
+                   absl::StrFormat("Error executing method %s: %s",
+                                   method_instance->method->name,
+                                   method_status.ToString()),
+                   c);
+    }
+  }
+}
+
+void RpcServer::SendRpcError(std::shared_ptr<RpcServer> server,
+                             std::shared_ptr<Session> session,
+                             std::shared_ptr<MethodInstance> method_instance,
+                             const RpcRequest &request,
+                             const std::string &error, co::Coroutine *c) {
+  subspace::RpcResponse response;
+  response.set_session_id(session->session_id);
+  response.set_request_id(request.request_id());
+  response.set_client_id(request.client_id());
+  response.set_error(error);
+
+  uint64_t length = response.ByteSizeLong();
+  absl::StatusOr<void *> buffer;
+  for (;;) {
+    buffer =
+        method_instance->response_publisher->GetMessageBuffer(int32_t(length));
+    if (!buffer.ok()) {
+      server->logger_.Log(toolbelt::LogLevel::kError,
+                          "Error getting buffer for error in method %s: %s",
+                          method_instance->method->name.c_str(),
+                          buffer.status().ToString().c_str());
+      response.set_error(absl::StrFormat(
+          "Error getting buffer for error in method %s: %s",
+          method_instance->method->name, buffer.status().ToString()));
+      return;
+    }
+    if (*buffer != nullptr) {
+      break;
+    }
+    if (!server->interrupt_pipe_.ReadFd().Valid()) {
+      return;
+    }
+    // Buffer is not ready, wait and try again.
+    auto status = method_instance->response_publisher->Wait(
+        server->interrupt_pipe_.ReadFd(), c);
+    if (!status.ok()) {
+      server->logger_.Log(toolbelt::LogLevel::kError,
+                          "Error waiting for buffer: %s",
+                          status.status().ToString().c_str());
+      return;
+    }
+    if (*status == server->interrupt_pipe_.ReadFd().Fd()) {
+      return;
+    }
+  }
+  // We got a buffer, fill it in and send it.
+  if (!response.SerializeToArray(*buffer, length)) {
+    server->logger_.Log(toolbelt::LogLevel::kError,
+                        "Error serializing response for method %s",
+                        method_instance->method->name.c_str());
+    return;
+  }
+
+  auto pub_result = method_instance->response_publisher->PublishMessage(length);
+  if (!pub_result.ok()) {
+    server->logger_.Log(toolbelt::LogLevel::kError,
+                        "Error publishing error response for method %s: %s",
+                        method_instance->method->name.c_str(),
+                        pub_result.status().ToString().c_str());
+  }
+}
+
+void RpcServer::SendStreamRpcResponse(
+    std::shared_ptr<RpcServer> server, std::shared_ptr<Session> session,
+    std::shared_ptr<MethodInstance> method_instance, const RpcRequest &request,
+    std::unique_ptr<google::protobuf::Any> result, bool is_last,
+    bool is_cancelled, co::Coroutine *c) {
+  // Send response back to the client each time this is called
+  subspace::RpcResponse response;
+  response.set_session_id(session->session_id);
+  response.set_request_id(request.request_id());
+  response.set_client_id(request.client_id());
+  if (result != nullptr) {
+    response.set_allocated_result(result.release());
+  }
+  response.set_is_last(is_last);
+  response.set_is_cancelled(is_cancelled);
+
+  uint64_t length = response.ByteSizeLong();
+  absl::StatusOr<void *> buffer;
+  for (;;) {
+    buffer =
+        method_instance->response_publisher->GetMessageBuffer(int32_t(length));
+    if (!buffer.ok()) {
+      server->logger_.Log(toolbelt::LogLevel::kError,
+                          "Error getting buffer for method %s: %s",
+                          method_instance->method->name.c_str(),
+                          buffer.status().ToString().c_str());
+      response.set_error(absl::StrFormat(
+          "Error getting buffer for method %s: %s",
+          method_instance->method->name, buffer.status().ToString()));
+      return;
+    }
+    if (*buffer != nullptr) {
+      break;
+    }
+    if (!server->interrupt_pipe_.ReadFd().Valid()) {
+      return;
+    }
+    // Buffer is not ready, wait and try again.
+    auto status = method_instance->response_publisher->Wait(
+        server->interrupt_pipe_.ReadFd(), c);
+    if (!status.ok()) {
+      server->logger_.Log(toolbelt::LogLevel::kError,
+                          "Error waiting for buffer: %s",
+                          status.status().ToString().c_str());
+      return;
+    }
+    if (*status == server->interrupt_pipe_.ReadFd().Fd()) {
+      return;
+    }
+  }
+  // We got a buffer, fill it in and send it.
+  if (!response.SerializeToArray(*buffer, length)) {
+    server->logger_.Log(toolbelt::LogLevel::kError,
+                        "Error serializing response for method %s",
+                        method_instance->method->name.c_str());
+    return;
+  }
+  server->logger_.Log(
+      toolbelt::LogLevel::kDebug, "Publishing response for method %s: %s",
+      method_instance->method->name.c_str(), response.DebugString().c_str());
+  auto pub_result = method_instance->response_publisher->PublishMessage(length);
+  if (!pub_result.ok()) {
+    server->logger_.Log(toolbelt::LogLevel::kError,
+                        "Error publishing response for method %s: %s",
+                        method_instance->method->name.c_str(),
+                        pub_result.status().ToString().c_str());
+  }
+  server->logger_.Log(toolbelt::LogLevel::kDebug,
+                      "Published response for ˝method %s",
+                      method_instance->method->name.c_str());
+}
+
+namespace internal {
+bool AnyStreamWriter::Write(std::unique_ptr<google::protobuf::Any> res,
+                            co::Coroutine *c) {
+  if (IsCancelled()) {
+    return false;
+  }
+  RpcServer::SendStreamRpcResponse(server, session, method_instance, request,
+                                   std::move(res), false, IsCancelled(), c);
+  return true;
+}
+
+void AnyStreamWriter::Finish(co::Coroutine *c) {
+  RpcServer::SendStreamRpcResponse(server, session, method_instance, request,
+                                   nullptr, true, IsCancelled(), c);
+}
+
+  void Method::MakeChannelNames(RpcServer *server) {
+    request_channel =
+        absl::StrFormat("/rpc/%s/%s/request", server->Name(), this->name);
+    response_channel =
+        absl::StrFormat("/rpc/%s/%s/response", server->Name(), this->name);
+    if (IsStreaming()) {
+      cancel_channel =
+          absl::StrFormat("/rpc/%s/%s/cancel", server->Name(), this->name);
+    }
+  }
+} // namespace internal
 } // namespace subspace
