183479c4f6afc3573b742282f73a27560627d5c9
Damjan Stulic <dstulic@gmail.com>
2025-10-30 13:38:44 -0700

    exposed channel properties via message (#40)
    Co-authored-by: dallison <david.s.allison@gmail.com>

--- common/channel.h
+++ common/channel.h
@@ -378,6 +378,7 @@ public:
   // Get the number of slots in the channel (can't be changed)
   int NumSlots() const { return num_slots_; }
   void SetNumSlots(int n) { num_slots_ = n; }
+  std::string SlotType() const { return type_; }
 
   void CleanupSlots(int owner, bool reliable, bool is_pub, int vchan_id);
 
