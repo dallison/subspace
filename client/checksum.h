bef76cf08b949ae7897a1b761d8161440cf982ba
Dave Allison <david.s.allison@gmail.com>
2025-11-26 14:40:24 -0800

    Formatting

--- client/checksum.h
+++ client/checksum.h
@@ -4,12 +4,12 @@
 
 #pragma once
 
-#include <cstdint>
-#include <cstddef>
 #include "absl/types/span.h"
+#include <cstddef>
+#include <cstdint>
 #include <vector>
 namespace subspace {
-    uint32_t CalculateChecksum(const std::vector<absl::Span<const uint8_t>>& data);
-    bool VerifyChecksum(const std::vector<absl::Span<const uint8_t>>& data, uint32_t checksum);
-}
-
+uint32_t CalculateChecksum(const std::vector<absl::Span<const uint8_t>> &data);
+bool VerifyChecksum(const std::vector<absl::Span<const uint8_t>> &data,
+                    uint32_t checksum);
+} // namespace subspace
